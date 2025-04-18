f18e7d31d7547783421e80536c9ae29f27ac7d1c
attacker frnd <warriorsshadow39@gmail.com>
2025-04-18 12:02:59 +0530

    done this second page

--- src/Component/Common/NextButton.jsx
+++ src/Component/Common/NextButton.jsx
@@ -4,10 +4,12 @@ const NextButton = ({ onClick, text = "Next", top = 839 }) => {
   return (
     <span 
       onClick={onClick}
-      className={`cursor-pointer box-border flex flex-row justify-center items-center p-[10px_35px] gap-[10px] absolute w-[350px] h-[39px] left-[162px] top-[${top}px] bg-gradient-to-r from-[rgba(48,90,255,0.8)] to-[#B5D2FF] rounded-[8px] text-center text-white`}>
+      style={{ top: `${top}px` }} // 👈 only this style is added
+      className="cursor-pointer box-border flex flex-row justify-center items-center p-[10px_35px] gap-[10px] absolute w-[350px] h-[39px] left-[162px] bg-gradient-to-r from-[rgba(48,90,255,0.8)] to-[#B5D2FF] rounded-[8px] text-center text-white"
+    >
       {text}
     </span>
   );
 };
 
-export default NextButton; 
+export default NextButton;
